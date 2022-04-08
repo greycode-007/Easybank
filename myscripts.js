2b51beda82bf05b8146b39920d781bbfa5f6ada9
stanley <stanleyanosike@gmail.com>
2022-04-08 15:41:58 -0700

    my first commit.

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+document.getElementsByClassName
+document.getElementById("rounded").onclick = function() {
+  
+  document.getElementById("circle").style.display = "block";
+}
